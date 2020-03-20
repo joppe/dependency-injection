ba74d9c0458a820c6b001f5bf8dcf90d747e3ecb
Joppe Aarts <joppe@apestaartje.info>
2020-03-20 10:23:51 +0100

    vs code debugging working with ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,28 @@
 module.exports = {
+    coverageDirectory: 'coverage',
+    globals: {
+        'ts-jest': {
+            tsConfig: {
+                inlineSourceMap: true,
+                module: 'commonjs',
+                paths: {
+                    '@apestaartje/dependency-injection/*': [
+                        'src/*',
+                    ],
+                },
+                sourceMap: false,
+            },
+        },
+    },
     moduleNameMapper: {
-        "^@apestaartje/dependency-injection/(.*)$": "<rootDir>/src/$1"
+        '^@apestaartje/dependency-injection/(.*)$': '<rootDir>/src/$1',
+    },
+    preset: 'ts-jest',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.jsx?$': 'babel-jest',
     },
     transformIgnorePatterns: [
-        "/node_modules/(?!@apestaartje).+(js|jsx)$"
+        '/node_modules/(?!@apestaartje).+(js|jsx)$',
     ],
 };
